f7eaf6a4ae517274c9fd83927d30122d96748a07
hak2711 <hak271111@gmail.com>
2021-02-03 14:45:21 +0900

    login page_link options&adjust details

---
+++ login_option.js
@@ -0,0 +1,7 @@
+function certificateLogin(){
+    var isChrome = /Chrome/.test(navigator.userAgent)&&/Google Inc/.test(navigator.vendor);
+    if (isChrome){
+        //alert('인증서 로그인하시려면 Internet Explorer를 이용하여 주십시오.');
+    }
+    alert('인증서 로그인하시려면 Internet Explorer를 이용하여 주십시오.');
+}
